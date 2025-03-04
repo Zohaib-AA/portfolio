9bcc69dfed6e8718f58ef7d50bb5e1530fb48591
Zohaib <ansarizohaibakhter@gmail.com>
2025-03-04 15:22:29 +0530

    feat: sections created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,16 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css','./mediaQueries.css']
 })
 export class AppComponent {
   title = 'portfolio';
+  myImage ='src/assets/p1.png'
+
+  toggleMenu(){
+    const menuLinks = document.querySelector('.menu-links'); 
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    menuLinks?.classList.toggle("open"); 
+    hamburgerIcon?.classList.toggle("open"); 
+  }
 }
